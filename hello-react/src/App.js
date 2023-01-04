f4e044c25030ae574fcd750e0a2895c143eaa04a
dbs1597 <dbs1597@naver.com>
2023-01-04 16:46:46 +0900

    6장 컴포넌트 반복

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,18 +3,12 @@ import React, { Component } from 'react';
 // import Say from './Say';
 // import EventPractice from './EventPractice';
 // import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+// import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
