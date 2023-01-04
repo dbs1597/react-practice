2218d6c73acbde70a5d8159325d906cc4fd35704
dbs1597 <dbs1597@naver.com>
2023-01-04 15:54:48 +0900

    5장 ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 // import MyComponent from './MyComponent';
 // import Say from './Say';
-import EventPractice from './EventPractice';
+// import EventPractice from './EventPractice';
+// import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
 
 //import './App.css';
 
-const App = () => {
-  return <EventPractice />;
-};
-
-export default App;
+// const App = () => {
+//   return <EventPractice />;
+// };
 
 // function App() {
 //   const name = 'react';
@@ -20,17 +35,3 @@ export default App;
 //     </header>
 //   );
 // }
-
-// class App extends Component {
-//   render() {
-//     const name = 'react';
-//     return (
-//       <header>
-//         <div className="react">{name}</div>
-//         <p></p>
-//       </header>
-//     );
-//   }
-// }
-
-
